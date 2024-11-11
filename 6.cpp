1df2f924a39775eba6c3732781e6629a2d89a140
darina_planck <dasamotorina2@gmail.com>
2024-11-11 14:43:15 +0300

    сделала заполнение массива рандомными цифрами

--- 6.cpp
+++ 6.cpp
@@ -6,5 +6,22 @@ int main(){
     int n = 0;
     cout << "Введите число N: ";
     cin >> n;
-    cout << "Это работает?"<< endl;
+    int random[n][n];
+    int start = 0;
+    int end = 9;
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= n; j++) {
+            random[i][j] = rand() % (end - start + 1) + start;
+        }
+    }
+    for (int i = 0;i <= n; i++){
+        for (int j = 0; j <= n; j++) {
+            cout << random[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    
+    
+    return 0;
 }
