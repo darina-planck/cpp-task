ef79da212c404e2f22e321920cfe804ef676fa95
darina_planck <dasamotorina2@gmail.com>
2024-11-11 13:47:19 +0300

    попытки в гитхаб 2

--- 6.cpp
+++ 6.cpp
@@ -6,10 +6,5 @@ int main(){
     int n = 0;
     cout << "Введите число N: ";
     cin >> n;
-    int start = 0;
-    int end = 10;
-    int x = rand() % (end - start + 1) + start;
-    cout << x << endl;
-    cout << x << endl;
-    return 0;
+    cout << "Это работает?"<< endl;
 }
