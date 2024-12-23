1afa2c02eb27b8160f8886f3c2d7dc2dc33a17f1
darina_planck <dasamotorina2@gmail.com>
2024-12-23 11:17:44 +0300

    перерешала 10 задачу

--- 10.cpp
+++ 10.cpp
@@ -4,48 +4,88 @@
 
 using namespace std;
 
-// Функция для распределения вещей по K рюкзакам с минимальной разницей в весах
-void distribute_items(int K, vector<int>& weights) {
-    int N = weights.size();
+vector<vector<int>> distribute_items_dp(vector<int>& items, int K) {
+    int total_weight = 0;
+    for (int item : items) {
+        total_weight += item;
+    }
+    int target_weight = total_weight / K;
+    int n = items.size();
 
-    // Рассортируем вещи по убыванию веса
-    sort(weights.rbegin(), weights.rend());
+    // Инициализируем матрицу для динамического программирования
+    vector<vector<bool>> dp(n + 1, vector<bool>(target_weight + 1, false));
+    dp[0][0] = true;
 
-    // Массив для хранения текущего веса каждого рюкзака
-    vector<int> rucksack_weights(K, 0);
-
-    // Распределяем вещи по рюкзакам с наименьшим текущим весом
-    for (int i = 0; i < N; ++i) {
-        // Находим рюкзак с минимальным текущим весом
-        int min_weight_idx = min_element(rucksack_weights.begin(), rucksack_weights.end()) - rucksack_weights.begin();
-        // Добавляем вещь в этот рюкзак
-        rucksack_weights[min_weight_idx] += weights[i];
+    // Заполняем матрицу
+    for (int i = 1; i <= n; ++i) {
+        for (int w = 0; w <= target_weight; ++w) {
+            dp[i][w] = dp[i-1][w];
+            if (w >= items[i-1]) {
+                dp[i][w] = dp[i][w] || dp[i-1][w - items[i-1]];
+            }
+        }
     }
 
-    // Выводим вес каждого рюкзака
-    cout << "Распределение вещей по рюкзакам:" << endl;
-    for (int i = 0; i < K; ++i) {
-        cout << "Рюкзак " << i + 1 << " (вес = " << rucksack_weights[i] << ")" << endl;
+    // Находим максимальный вес, который можно собрать
+    int max_weight = 0;
+    for (int w = 0; w <= target_weight; ++w) {
+        if (dp[n][w]) {
+            max_weight = w;
+        }
     }
+
+    // Восстанавливаем набор вещей для первого рюкзака
+    vector<int> backpack1;
+    int w = max_weight;
+    for (int i = n; i > 0; --i) {
+        if (dp[i][w] && !dp[i-1][w]) {
+            backpack1.push_back(items[i-1]);
+            w -= items[i-1];
+        }
+    }
+
+    // Оставшиеся вещи идут во второй рюкзак
+    vector<int> backpack2;
+    for (int item : items) {
+        if (find(backpack1.begin(), backpack1.end(), item) == backpack1.end()) {
+            backpack2.push_back(item);
+        }
+    }
+
+    return {backpack1, backpack2};
 }
 
 int main() {
     int N, K;
-
-    // Ввод количества вещей и числа рюкзаков
     cout << "Введите количество вещей: ";
     cin >> N;
-    cout << "Введите количество рюкзаков (K): ";
-    cin >> K;
 
-    vector<int> weights(N);
+    vector<int> items(N);
     for (int i = 0; i < N; ++i) {
         cout << "Введите вес вещи " << i + 1 << ": ";
-        cin >> weights[i];
+        cin >> items[i];
     }
 
-    // Проверяем возможность распределения вещей по K рюкзакам
-    distribute_items(K, weights);
+    cout << "Введите количество рюкзаков: ";
+    cin >> K;
+
+    // Распределяем вещи по рюкзакам
+    vector<vector<int>> backpacks = distribute_items_dp(items, K);
+
+    // Выводим результат
+    cout << "Распределение вещей по рюкзакам:" << endl;
+    for (int i = 0; i < backpacks.size(); ++i) {
+        int weight = 0;
+        for (int item : backpacks[i]) {
+            weight += item;
+        }
+        cout << "Рюкзак " << i + 1 << ": " << weight << " кг" << endl;
+        cout << "Вещи: ";
+        for (int item : backpacks[i]) {
+            cout << item << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
