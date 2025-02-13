76c451e6ae96a7b2c35ca0928ba9b1e46e5b922f
darina_planck <dasamotorina2@gmail.com>
2025-02-13 16:41:01 +0300

    начало положено

---
+++ 13.fshp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// Константы
+const int pence_in_shilling = 12;
+const int shilling_in_pound = 20;
+const int pence_in_pound = pence_in_shilling * shilling_in_pound;
+
